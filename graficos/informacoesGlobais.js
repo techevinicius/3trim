d663dbd66d3bfae445eaa8d7cd112e0d181d0bd7
techevinicius <108283361+techevinicius@users.noreply.github.com>
2024-10-18 11:43:36 -0300

    css

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,6 +1,6 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function vizualizarinformacoesGlobais() {
+async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
@@ -17,4 +17,4 @@ async function vizualizarinformacoesGlobais() {
     container.appendChild(paragrafo)
 }
 
-vizualizarinformacoesGlobais()
+vizualizarInformacoesGlobais()
